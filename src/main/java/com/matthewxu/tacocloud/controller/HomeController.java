49d1ad51a43053440549d2f0e620b16d8ffa6473
Matthew Xu <matthew.xu@carel.locale>
2021-02-26 15:50:55 +0800

    Initial commit

---
+++ src/main/java/com/matthewxu/tacocloud/controller/HomeController.java
@@ -0,0 +1,21 @@
+
+package com.matthewxu.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description:
+ * @author Matthew Xu
+ * @date Feb 26, 2021
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping("home")
+	public String home(){
+		return "home";
+	}
+}
